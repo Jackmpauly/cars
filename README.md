868703fc7eb7597f86af07993acdfb66a679ed4e
Jack Pauly <pauly@chapman.edu>
2019-11-22 11:00:34 -0800

    Added cars.sh, My_old_cars, and updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 This repo contains a bash script named “cars.sh” that will help a user maintain an inventory of old cars.
  
  The user will be presented with a menu of three options:
+
+
+	#Alt-1. ADD a NEW CAR. Script asks for year, make, and model of new car
+	#================================================
+	#Alt-2. DISPLAY the list of cars
+	#================================================
+	#Alt-3. EXIT THE PROGRAM
+	#================================================
---
+++ cars.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# cars.sh
+# *JACK PAULY*
+# 11.22.19
+
+quit=0
+input="/cars/My_old_cars"
+
+
+
+while [ $quit -eq 0 ]
+do
+	echo "Type the number 1 to enter a new car"
+	echo "Type the number 2 to display the list of cars"
+	echo "Type the number 3 to exit the program"
+	read SEL
+	if [ $SEL -eq 1 ]
+	then
+		echo "What year is the car?"
+		read YEAR
+		echo "What company is the car from?"
+		read COMP
+		echo "What make is the car?"
+		read MAKE
+		NEWCAR=$YEAR:$COMP:$MAKE
+		echo $NEWCAR >> My_old_cars
+		echo added $NEWCAR
+	fi
+	if [ $SEL -eq 2 ]
+	then
+		sort My_old_cars
+		echo My_old_cars
+	fi
+	if [ $SEL -eq 3 ]
+	then
+		echo "Goodbye!"
+		quit=3
+	fi
+done
